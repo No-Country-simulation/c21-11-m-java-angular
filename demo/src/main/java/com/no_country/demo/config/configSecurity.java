3e3083d2b16a22d4f640500601a3519d790f3a95
alelex10 <elvizvida@gmail.com>
2024-10-29 19:07:48 -0300

    cors

--- demo/src/main/java/com/no_country/demo/config/configSecurity.java
+++ demo/src/main/java/com/no_country/demo/config/configSecurity.java
@@ -1,4 +1,18 @@
 package com.no_country.demo.config;
 
-public class configSecurity {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class configSecurity implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("*") // Origen permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los headers
+                .allowCredentials(true); // Permitir cookies si es necesario
+    }
 }
